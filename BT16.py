13029289de5de3ce28594a6a9bbd033c21e70fd8
thang1230 <72205096+Hoang-Quang-Thang@users.noreply.github.com>
2021-05-04 10:52:25 +0700

    Add files via upload

---
+++ BT16.py
@@ -0,0 +1,12 @@
+def bubblesort(list):
+    for iter_num in range(len(list)-1,0,-1):
+        for i in range(iter_num):
+            if list[i]>list[i+1]:
+                temp = list[i]
+                list[i] = list[i+1]
+                list[i+1] = temp
+
+
+list = [19,2,31,45,6,11,121,27]
+bubblesort(list)
+print(list)
