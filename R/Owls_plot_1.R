64be0fff291ff93ad2d27583d67372b76465e931
thang1230 <72205096+Hoang-Quang-Thang@users.noreply.github.com>
2021-05-11 10:40:03 +0700

    Add files via upload

---
+++ R/Owls_plot_1.R
@@ -0,0 +1,24 @@
+setwd('C:\\Users\\RED\\R') # set working file
+Owls <- read.table('Owls.txt', header = TRUE) # extract file .txt
+
+AllNest <- unique(Owls$Nest) #Check how many Nest in dataset
+N <- length(AllNest)  # length of AllNest
+
+sibl <- Owls$SiblingNegotiation
+sibl[Owls$SiblingNegotiation<=17] <- 4 # set pch 
+sibl[Owls$SiblingNegotiation>17] <- 3
+
+for (i in 1:N){         # check each row of AllNest
+  Nest.i <- AllNest[i]  # nest from 1 to N
+  A.i <- Owls[Owls$Nest == Nest.i, ]  # check Nest column
+  YourFileName <- paste(Nest.i, '.jpg', sep = '') # name the file
+  jpeg(file = YourFileName)  # picture file (.jpeg)
+  plot(x = A.i$SiblingNegotiation,    # variable x
+       y = A.i$ArrivalTime,           # variable y
+       xlab = 'Sibling Negotiation',  # x name
+       main = Nest.i,                 # plot name
+       ylab = 'Arrival Time',         # y name
+       pch=sibl)                      # pch
+  dev.off()                           # end
+}
+
---
+++ R/Owls_plot_2.R
@@ -0,0 +1,24 @@
+setwd('C:\\Users\\RED\\R') # set working file
+Owls <- read.table('Owls.txt', header = TRUE)  # extract file .txt
+
+Owls$NestNight <- paste(Owls$Nest,  # name of Nest column
+                        Owls$FoodTreatment,  # name of FoodTreatment column
+                        sep = '_')  # combine name of 2 columns
+
+AllNestsNights <- unique(Owls$NestNight) #Check how many NestNight
+                                         #in dataset
+N <- length(AllNestsNights) # length of AllNestsNights
+for (i in 1:N){   # check each row of AllNestsNights
+  NestNight.i <- AllNestsNights[i] # nest from 1 to N
+  
+  Owlsi <- Owls[Owls$NestNight == NestNight.i ,]  # check NestNight column
+  YourFileName <- paste(NestNight.i, '.jpg', sep = '') # name the file
+  jpeg(file = YourFileName)  # picture file (.jpeg)
+  plot(x = Owlsi$ArrivalTime ,       # variable x
+       y = Owlsi$NegPerChick,        # variable y
+       xlab = 'Arrival Time',        # x name
+       ylab = 'Sibling negotiation', # y name
+       main = NestNight.i            # plot name
+       )
+  dev.off()                          # end
+}
