74adfe54b41d2d7b94ea356fe1adeeb0d8f15f3f
thang1230 <72205096+Hoang-Quang-Thang@users.noreply.github.com>
2021-05-11 10:43:01 +0700

    Update Owls_plot_2.R

--- R/Owls_plot_2.R
+++ R/Owls_plot_2.R
@@ -8,7 +8,7 @@ Owls$NestNight <- paste(Owls$Nest,  # name of Nest column
 AllNestsNights <- unique(Owls$NestNight) #Check how many NestNight
                                          #in dataset
 N <- length(AllNestsNights) # length of AllNestsNights
-for (i in 1:N){   # check each row of AllNestsNights
+for (i in 1:N){   # check lines of AllNestsNights
   NestNight.i <- AllNestsNights[i] # nest from 1 to N
   
   Owlsi <- Owls[Owls$NestNight == NestNight.i ,]  # check NestNight column
