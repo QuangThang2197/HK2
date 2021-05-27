3a4a9db047dd26572e596e87507a9d435f5c7d8b
thang1230 <72205096+Hoang-Quang-Thang@users.noreply.github.com>
2021-05-27 16:07:42 +0700

    Update plot-function.R

--- R/plot-function.R
+++ R/plot-function.R
@@ -7,12 +7,12 @@ Benthic <-read.table(file="RIKZ2.txt", header=TRUE)
 Benthic$fBeach <- factor(Benthic$Beach)
 plot(Benthic$Richness ~ Benthic$fBeach)
 
-plot.new() #tao ra 1 trang moi de ve bieu do moi
-win.graph  #tao 1 cua so va cho phep ve bieu do vao cua so do
-windows    #tuong tu win.graph
-savePlot   #luu file vao thiet bi cua window
-locator #xac dinh cac diem minh chon tren bieu do de hien thi
-matplot #dem du lieu dau vao va ve chung len bieu do
-persp # ve bieu do 3D hinh hop
-cut  # cho biet gia tri nam trong khoang nao
-split# chia va gom cac gia tri co cung stt lai voi nhau
+plot.new() # tạo ra 1 trang mới để vẽ biểu đồ mới
+win.graph  # tạo 1 cửa sổ và cho phép vẽ biểu đồ vào cửa sổ đó
+windows    # tương tự win.graph
+savePlot   # lưu file vào thiết bị của window
+locator    # xác định các điểm được chọn trên biểu đồ để hiển thị
+matplot    # đem dữ liệu đầu vào và vẽ chúng lên biểu đồ
+persp      # vẽ biểu đồ 3D hình hộp 
+cut        # cho biết giá trị nằm trong khoảng nào
+split      # chia và gom các giá trị có cùng stt lại với nhau
