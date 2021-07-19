21e89e8aa616326aa1939f6b286e6180830d1a51
Th@ng12350 <72205096+QuangThang2197@users.noreply.github.com>
2021-07-19 13:45:46 +0700

    Update BT02_UocSoChungLonNhat.R

--- CTDLGT_R/BT02_UocSoChungLonNhat.R
+++ CTDLGT_R/BT02_UocSoChungLonNhat.R
@@ -1,13 +1,12 @@
-
 uscln <- function(a, b){
   if (a < 0 | b < 0){
-    return('None exists')
+    return('None exists') # khi 1 trong 2 so be hon 0 thi ko ton tai giai thua
   }
   if (b == 0){
-    return(a)
+    return(a)     # khi b = 0  thi chi con a nen giai thua la a
   }
   else {
-    return(uscln(b, a %% b))
+    return(uscln(b, a %% b)) # uscln thi ta lay phan du cua a chia b
   }
 }
 
