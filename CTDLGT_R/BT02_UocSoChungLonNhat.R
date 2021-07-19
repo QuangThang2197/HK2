22d72070ccc7ff14ef918f477d740e30f4c3224f
Th@ng12350 <72205096+QuangThang2197@users.noreply.github.com>
2021-07-19 13:52:22 +0700

    Update BT02_UocSoChungLonNhat.R

--- CTDLGT_R/BT02_UocSoChungLonNhat.R
+++ CTDLGT_R/BT02_UocSoChungLonNhat.R
@@ -1,8 +1,5 @@
 uscln <- function(a, b){
-  if (a < 0 | b < 0){
-    return('None exists') # khi 1 trong 2 so be hon 0 thi ko ton tai giai thua
-  }
-  if (b == 0){
+  if (b <= 0){
     return(a)     # khi b = 0  thi chi con a nen giai thua la a
   }
   else {
