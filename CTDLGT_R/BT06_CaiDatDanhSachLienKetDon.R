8b022e9d80b7968d40e9817df25921132f17035d
Th@ng12350 <72205096+QuangThang2197@users.noreply.github.com>
2021-07-19 09:12:53 +0700

    Update BT06_CaiDatDanhSachLienKetDon.R

--- CTDLGT_R/BT06_CaiDatDanhSachLienKetDon.R
+++ CTDLGT_R/BT06_CaiDatDanhSachLienKetDon.R
@@ -1,5 +1,5 @@
-lst <- list() # creates an empty (length zero) list
-lst[[1]] <- 1 # automagically extends the lst
+lst <- list() # tao list rong
+lst[[1]] <- 1 # mo rong list bang cach gan
 lst[[2]] <- 2 
 lst[[3]] <- 3 
 lst[[4]] <- 4 
