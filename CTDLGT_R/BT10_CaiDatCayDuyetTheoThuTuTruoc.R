fe367f4dc85805b390578bbe7d476d16a720bcaa
Th@ng12350 <72205096+QuangThang2197@users.noreply.github.com>
2021-07-19 13:48:04 +0700

    Update BT10_CaiDatCayDuyetTheoThuTuTruoc.R

--- CTDLGT_R/BT10_CaiDatCayDuyetTheoThuTuTruoc.R
+++ CTDLGT_R/BT10_CaiDatCayDuyetTheoThuTuTruoc.R
@@ -1,17 +1,17 @@
 install.packages('data.tree')
 library(data.tree)
 # thiet lap cay
-A <- Node$new("A")
-B <- A$AddChild("B")
-C <- B$AddChild("C")
-D <- B$AddChild("D")
-E <- A$AddChild("E")
-F <- E$AddChild("F")
-G <- E$AddChild("G")
-H <- A$AddChild("H")
-I <- H$AddChild("I")
-J <- H$AddChild("J")
-K <- H$AddChild("K")
+A <- Node$new("A")      # A là đầu tiên
+B <- A$AddChild("B")    # B là con của A
+C <- B$AddChild("C")    # c là con của B
+D <- B$AddChild("D")    # D là con của B
+E <- A$AddChild("E")    # E là con của A
+F <- E$AddChild("F")    # F là con của E
+G <- E$AddChild("G")    # G là con của E
+H <- A$AddChild("H")    # H là con của A
+I <- H$AddChild("I")    # I là con của H
+J <- H$AddChild("J")    # J là con của H
+K <- H$AddChild("K")    # K là con của H
 print(A)
 
 A$Get('level')
